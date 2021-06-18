8bf9140a0bee497fae4a6d10bce29dd249ca88a5
seynurmammadov <seynursm@code.edu.az>
2021-06-18 13:47:48 +0400

    fix

---
+++ myauto/src/main/java/az/code/myauto/MyautoApplication.java
@@ -0,0 +1,13 @@
+package az.code.myauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyautoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyautoApplication.class, args);
+    }
+
+}
