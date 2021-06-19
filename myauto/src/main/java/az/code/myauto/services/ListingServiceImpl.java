6f88c590cd38204a8738a06080f392d993420cfc
seynurmammadov <seynursm@code.edu.az>
2021-06-19 18:56:43 +0400

    MYAUT-87 create listing interface fix pom fix MailSender

--- myauto/src/main/java/az/code/myauto/services/ListingServiceImpl.java
+++ myauto/src/main/java/az/code/myauto/services/ListingServiceImpl.java
@@ -5,9 +5,12 @@ import az.code.myauto.models.Listing;
 import az.code.myauto.models.UserData;
 import az.code.myauto.models.dtos.ListingCreationDTO;
 import az.code.myauto.models.dtos.ListingGetDTO;
+import az.code.myauto.models.dtos.ListingListDTO;
 import az.code.myauto.services.interfaces.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ListingServiceImpl implements ListingService {
@@ -21,6 +24,51 @@ public class ListingServiceImpl implements ListingService {
     @Override
     public ListingGetDTO create(ListingCreationDTO listing, UserData user) {
         return new ListingGetDTO(listingDAO.create(new Listing(listing,user)));
+    }
+
+    @Override
+    public ListingGetDTO update(long id, ListingCreationDTO listing, UserData user) {
+        return null;
+    }
+
+    @Override
+    public ListingGetDTO delete(long id, UserData user) {
+        return null;
+    }
+
+    @Override
+    public ListingGetDTO makeVip(long id, UserData user) {
+        return null;
+    }
+
+    @Override
+    public ListingGetDTO makePaid(long id, UserData user) {
+        return null;
+    }
+
+    @Override
+    public ListingGetDTO getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<ListingListDTO> getListings() {
+        return null;
+    }
+
+    @Override
+    public List<ListingListDTO> getUserListingsByUsername() {
+        return null;
+    }
+
+    @Override
+    public List<ListingListDTO> getUserListings(UserData userData) {
+        return null;
+    }
+
+    @Override
+    public ListingGetDTO getUserListingById(long id, UserData userData) {
+        return null;
     }
 
 
