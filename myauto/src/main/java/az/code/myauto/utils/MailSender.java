d5661f967d0a0b6d66cc005ffcc4b1d72e079357
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-19 13:19:41 +0400

    MYAUT-75-creating-mailsender-and-pagination-utils added

--- myauto/src/main/java/az/code/myauto/utils/MailSender.java
+++ myauto/src/main/java/az/code/myauto/utils/MailSender.java
@@ -1,5 +1,21 @@
 package az.code.myauto.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class MailSender {
+    @Autowired
+    private static JavaMailSender javaMailSender;
+
+
+
+    public void sendEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        javaMailSender.send(message);
+    }
 
 }
