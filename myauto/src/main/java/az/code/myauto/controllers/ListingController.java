4e04aca45c3cae89679faae30ee314a40b6cba6b
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-19 19:23:00 +0400

    MYAUT-86-creating-listing-controller some changes and logger was added

--- myauto/src/main/java/az/code/myauto/controllers/ListingController.java
+++ myauto/src/main/java/az/code/myauto/controllers/ListingController.java
@@ -2,9 +2,13 @@ package az.code.myauto.controllers;
 
 import az.code.myauto.models.dtos.ListingGetDTO;
 import az.code.myauto.services.interfaces.ListingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/listings")
@@ -16,15 +20,24 @@ public class ListingController {
         this.listingService = listingService;
     }
 
+    Logger logger = LoggerFactory.getLogger(ListingController.class);
+
     @GetMapping("/{id}")
     public ResponseEntity<ListingGetDTO> getListingById(@PathVariable long id) {
-        //TODO after updating ListingService, change null
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        logger.info("Getting listing (by id) by unregistered user");
+        return new ResponseEntity<>(listingService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getListings() {
+        logger.info("Getting listings by unregistered user");
+        return new ResponseEntity<>(listingService.getListings(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}/images/{imageId}")
     public ResponseEntity<ListingGetDTO> getListingImageById(@PathVariable long id,
                                                              @PathVariable long imageId) {
+        logger.info("Getting listing image by unregistered user");
         //TODO after updating ListingService, change null
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
--- myauto/src/main/java/az/code/myauto/controllers/ProfileController.java
+++ myauto/src/main/java/az/code/myauto/controllers/ProfileController.java
@@ -4,6 +4,8 @@ import az.code.myauto.models.UserData;
 import az.code.myauto.models.dtos.ListingCreationDTO;
 import az.code.myauto.models.dtos.ListingGetDTO;
 import az.code.myauto.services.interfaces.ListingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +20,32 @@ public class ProfileController {
         this.listingService = listingService;
     }
 
+
+    Logger logger = LoggerFactory.getLogger(ProfileController.class);
+
     @PostMapping("/listings")
-    public ResponseEntity<ListingGetDTO> getUser(@RequestBody ListingCreationDTO listingCreationDTO, @RequestAttribute UserData user) {
+    public ResponseEntity<ListingGetDTO> getUser(@RequestBody ListingCreationDTO listingCreationDTO,
+                                                 @RequestAttribute UserData user) {
+        logger.info("Posting new listing by registered user");
         return new ResponseEntity<>(listingService.create(listingCreationDTO,user), HttpStatus.CREATED);
     }
+
+    @PutMapping("/listings/{id}")
+    public ResponseEntity<ListingGetDTO> updateUser(@PathVariable long id,
+                                                    @RequestBody ListingCreationDTO listingCreationDTO,
+                                                    @RequestAttribute UserData user) {
+        logger.info("Updating listing by registered user");
+        return new ResponseEntity<>(listingService.update(id, listingCreationDTO, user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/listings/{id}")
+    public ResponseEntity<ListingGetDTO> deleteUser(@PathVariable long id,
+                                                    @RequestAttribute UserData user) {
+        logger.info("Deleting listing by registered user");
+        return new ResponseEntity<>(listingService.delete(id, user), HttpStatus.OK);
+    }
+
+
+
+
 }
