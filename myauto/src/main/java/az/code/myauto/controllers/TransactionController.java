b466e7d019b3ff80666955c69d889cfdbc3d3485
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-20 17:30:24 +0400

    MYAUT-121-update-transaction-controller added

--- myauto/src/main/java/az/code/myauto/controllers/TransactionController.java
+++ myauto/src/main/java/az/code/myauto/controllers/TransactionController.java
@@ -1,35 +1,42 @@
 package az.code.myauto.controllers;
 
+import az.code.myauto.exceptions.TransactionIncorrectAmountException;
 import az.code.myauto.models.Transaction;
 import az.code.myauto.models.UserData;
-import az.code.myauto.models.dtos.SubscriptionListDto;
+import az.code.myauto.services.interfaces.TransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/profile")
 public class TransactionController {
+    final
+    TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 
     Logger logger = LoggerFactory.getLogger(TransactionController.class);
+
+
 
     @GetMapping("/wallet")
     public ResponseEntity<Double> getBalance(@RequestAttribute UserData user) {
         logger.info("Getting balance by registered user");
-        //TODO after updating TransactionService, 'null' must be changed.
-        return new ResponseEntity<>(null, HttpStatus.OK);
+
+        return new ResponseEntity<>(transactionService.getBalance(user), HttpStatus.OK);
     }
 
     @PutMapping("/wallet/increase")
     public ResponseEntity<Transaction> increaseBalance(@RequestAttribute UserData user,
-                                                       @RequestParam Double amount ) {
+                                                       @RequestParam Double amount ) throws TransactionIncorrectAmountException {
         logger.info("Increasing balance by registered user");
-        //TODO after updating TransactionService, 'null' must be changed.
-        return new ResponseEntity<>(null, HttpStatus.OK);
+
+        return new ResponseEntity<>(transactionService.increaseBalance(amount, user), HttpStatus.OK);
     }
 
 
