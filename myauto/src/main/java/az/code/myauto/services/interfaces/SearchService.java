344914f8906f988d9d1eb7fd93a72af7b458c006
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-20 13:05:42 +0400

    MYAUT-118-creating-subscription-and-search-services almost done

--- myauto/src/main/java/az/code/myauto/services/interfaces/SearchService.java
+++ myauto/src/main/java/az/code/myauto/services/interfaces/SearchService.java
@@ -1,4 +1,28 @@
 package az.code.myauto.services.interfaces;
 
+import az.code.myauto.models.dtos.CityDTO;
+import az.code.myauto.models.dtos.ListingListDTO;
+import az.code.myauto.models.dtos.MakeDTO;
+import az.code.myauto.models.dtos.ModelDTO;
+import az.code.myauto.models.enums.BodyType;
+import az.code.myauto.models.enums.FuelType;
+
+import java.util.List;
+
 public interface SearchService {
+
+    List<ModelDTO> getAllModelsByMake(long makeId);
+
+    List<MakeDTO> getAllMakes();
+
+    List<FuelType> getAllFuelTypes();
+
+    List<BodyType> getAllBodyTypes();
+
+    List<CityDTO> getAllCities();
+
+    //TODO writing search parameters in better way
+//    List<ListingListDTO> search();
+
+
 }
