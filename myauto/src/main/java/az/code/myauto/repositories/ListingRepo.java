36f87927fc606cfc99c389ce718ba57939888595
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-21 13:43:44 +0400

    MYAUT-91-send-email-in-transaction done

--- myauto/src/main/java/az/code/myauto/repositories/ListingRepo.java
+++ myauto/src/main/java/az/code/myauto/repositories/ListingRepo.java
@@ -7,10 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface ListingRepo extends JpaRepository<Listing, Long> {
