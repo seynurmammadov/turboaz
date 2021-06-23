3f9e265fbd1b7e4b095d923ff96a948c812ad8b2
Asadullah Jamalov <[asadullahnj@code.edu.az]>
2021-06-23 15:51:04 +0400

    MYAUT-159-optimizing-messageutil done

--- myauto/src/main/java/az/code/myauto/utils/MessageUtil.java
+++ myauto/src/main/java/az/code/myauto/utils/MessageUtil.java
@@ -1,47 +1,67 @@
 package az.code.myauto.utils;
 
 import az.code.myauto.models.User;
-import az.code.myauto.models.UserConfirmationToken;
-import az.code.myauto.models.dtos.UserRegistrationDTO;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Component
 public class MessageUtil {
+
+    @Value("${mail.registration.success.subject}")
+    private String successMessageSubject;
+
+    @Value("${mail.registration.success.message}")
+    private String successMessageText;
+
+    @Value("${mail.registration.complete.subject}")
+    private String registrationVerifySubject;
+
+    @Value("${mail.registration.success.message}")
+    private String registrationVerifyText;
+
+    @Value("${mail.notification.subject}")
+    private String autoPaymentNotificationSubject;
+
+    @Value("${mail.notification.message}")
+    private String autoPaymentNotificationText;
+
+    @Value("${mail.subject}")
+    private String transactionNotificationSubject;
+
+    @Value("${mail.amount.info.message}")
+    private String transactionNotificationText;
+
+    @Value("${mail.balance.info.message}")
+    private String balanceNotificationText;
+
     final
     MailSenderUtil mailSenderUtil;
 
-    final
-    Environment environment;
 
-    public MessageUtil(MailSenderUtil mailSenderUtil, Environment environment) {
+    public MessageUtil(MailSenderUtil mailSenderUtil) {
         this.mailSenderUtil = mailSenderUtil;
-        this.environment = environment;
     }
 
     public void transactionsNotification(String type, User user, Double amount) {
-        String subject = type + environment.getProperty("mail.subject");
-        String content = user.getName() + environment.getProperty("mail.amount.info.message")
-                + amount + environment.getProperty("mail.balance.info.message") + user.getBalance();
+        String subject = type + transactionNotificationSubject;
+        String content = user.getName() + transactionNotificationText
+                + amount + balanceNotificationText + user.getBalance();
         mailSenderUtil.sendEmail(user.getEmail(), subject, content);
     }
 
     public void autoPaymentNotification(String to, LocalDateTime date) {
-        mailSenderUtil.sendEmail(to, environment.getProperty("mail.notification.subject"),
-                environment.getProperty("mail.notification.message") + date.toString());
+        mailSenderUtil.sendEmail(to, autoPaymentNotificationSubject,
+                autoPaymentNotificationText + date.toString());
     }
 
     public void regVerifyNotification(User user, String token) {
-        mailSenderUtil.sendEmail(user.getEmail(),
-                environment.getProperty("mail.registration.complete.subject"),
-                environment.getProperty("mail.registration.complete.message") + token);
+        mailSenderUtil.sendEmail(user.getEmail(), registrationVerifySubject,
+                registrationVerifyText + token);
     }
 
-    public void successRegister(User user){
-        mailSenderUtil.sendEmail(user.getEmail(),
-                environment.getProperty("mail.registration.success.subject"),
-                environment.getProperty("mail.registration.success.message"));
+    public void successRegister(User user) {
+        mailSenderUtil.sendEmail(user.getEmail(), successMessageSubject, successMessageText);
     }
 }
